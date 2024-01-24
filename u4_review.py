6d02673bc87335dc636a12193c2c4dd66b3cce10
lmendoza0506 <112989054+lmendoza0506@users.noreply.github.com>
2024-01-24 20:01:56 +0000

    1/2 of U4 Review

--- u4_review.py
+++ u4_review.py
@@ -11,4 +11,18 @@
 
 # 2) Creating Sprites with Surfaces and Rectangles
 
-#
+# SURFACE - a canvas where graphics can be drawn
+# images can be loaded onto a Surface, or a blank one can be used for drawing
+
+# RECTANGLE - rectangular area defined by its position and size
+# used to manage position and dimensions of a Surface
+
+# img_surf = pygame.Surface((width, height))
+# img_rect = img_surface.get_rect()
+
+# Game Loop -> Render image
+# screen.blit(img_surf, img_rect)
+
+# DRAW - add graphics or shapes directly to a surface
+# BLIT - update screen by copying the contents of a img_surface to the
+# screen at the specific position of img_rect
