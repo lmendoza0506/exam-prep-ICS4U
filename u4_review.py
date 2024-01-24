dd8b9acbe740b7b131013a5366bfaa01d4b2b235
lmendoza0506 <112989054+lmendoza0506@users.noreply.github.com>
2024-01-24 16:34:15 +0000

    Q1 done

--- u4_review.py
+++ u4_review.py
@@ -1,1 +1,14 @@
-# 1) Class
+# 1) Key Stages in a Game Loop
+
+# GAME LOOP - for loop composed of four key stages
+# I: Process user input
+# II: Update state of game objects, 
+# III: Render/draw update game state and create display/audio output
+# IV: Control frame rate (speed of loop iteration) of game
+
+# FRAME - each cycle of game loop (occurs multiple times in a second)
+# continue until condition to exit game is met
+
+# 2) Creating Sprites with Surfaces and Rectangles
+
+#
