9ab209d32deeb1a365cc379079ab220c7ccb5dbf
lmendoza0506 <112989054+lmendoza0506@users.noreply.github.com>
2024-01-25 20:14:28 +0000

    DONE U4

--- u4_review.py
+++ u4_review.py
@@ -26,3 +26,42 @@
 # DRAW - add graphics or shapes directly to a surface
 # BLIT - update screen by copying the contents of a img_surface to the
 # screen at the specific position of img_rect
+
+# 3) Sprites and Sprite Groups
+
+# SPRITE - 2D image of elements in game environment that can be moved and interacted with
+# common attributes include position and speed
+# pygame provides a Sprite class that
+## create multiple instances
+## give access to its built-in methods
+## able to extend for new methods
+
+# NOTE: main module/library - pygame, module - sprite, class - Group
+
+# SPRITE GROUPS - pygame.sprite.Group() is useful because:
+## container that manages Sprite opjects
+## include methods for collision detection
+## update position and render multiple sprites easier
+
+# COLLISION DETECTION - checking whether two or more game objects are overlapping
+
+# pygame.sprite.spritecollideany() is a useful method:
+## arguments are a Sprite and a Group
+## check every object in the Group if its .rect() intersects with the rest of the sprite
+## returns True if detected and False otherwise
+
+# IMAGE; pygame.image.load('file_name.png') -  load image and transfer to a Surface
+## .convert() optimizes the Surface and making future .blit() calls faster
+
+# 4) User Events and its Applications
+
+# USEREVENT - used to define their own events with custom identifies
+## trigger specific actions in response to certain game conditions
+
+# Defining a USEREVENT
+# custome_event_1 = pygame.USEREVENT + 1
+
+# pygame.event.get() - returns list of events in the event queue
+# pygame.event.get_pressed() - returns dict of event in event queue
+
+
